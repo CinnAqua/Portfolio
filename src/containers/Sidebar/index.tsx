79a2646eb814af9b900b0cd3d81f21e81ea6c071
CinnAqua <miguelcp1008@gmail.com>
2024-11-05 21:49:11 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/CinnAqua.png" />
-    <Titulo fontSize={20}>Miguel</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Miguel</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        CinnAqua
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
